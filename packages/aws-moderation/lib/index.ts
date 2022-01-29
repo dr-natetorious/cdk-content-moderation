c2bd821fb3074ee5603099ec2a0cbe5b20299fdd
natebachmeier <nate@bachmeier>
2022-01-29 00:29:08 -0500

    hrm..

--- packages/aws-moderation/lib/index.ts
+++ packages/aws-moderation/lib/index.ts
@@ -1,4 +1,5 @@
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface AwsModerationProps {
@@ -9,6 +10,9 @@ export class AwsModeration extends Construct {
 
   constructor(scope: Construct, id: string, props: AwsModerationProps = {}) {
     super(scope, id);
+
+
+    var bucket = new s3.Bucket(this,'MyBucket')
 
     // Define construct contents here
 
--- packages/sample-app/lib/sample-app-stack.ts
+++ packages/sample-app/lib/sample-app-stack.ts
@@ -1,10 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import { AwsModeration} from '../../aws-moderation/lib/index'
+//import * as m  from '@aws-moderation/'
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SampleAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new AwsModeration(this,'Moderation')
 
     // The code that defines your stack goes here
 
